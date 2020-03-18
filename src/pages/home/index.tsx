19d37114d83937c1b06143731d2b94e000c87971
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-18 16:06:29 +0000

    Built and styled homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./index.scss";
 
 const Home = () => {
-  return <div>I am a homepage test!</div>;
+  return (
+    <div>
+      <main>
+        <div className="banner">
+          <h1>PokéAPI</h1>
+          <h2>The RESTful Pokémon API</h2>
+          <p>Serving over 17,000,000 API calls each month!</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
