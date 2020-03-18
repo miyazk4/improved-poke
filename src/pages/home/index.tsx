0566dd3e731eaec1c439f26f6d801643580f6b80
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-18 18:52:35 +0000

    Added features and styles to homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.scss";
 
 const Home = () => {
   return (
     <div>
       <main>
-        <div className="banner">
-          <h1>PokéAPI</h1>
-          <h2>The RESTful Pokémon API</h2>
-          <p>Serving over 17,000,000 API calls each month!</p>
+        <div className="bannerContainer">
+          <div className="banner">
+            <h1>PokéAPI</h1>
+            <h2>The RESTful Pokémon API</h2>
+            <p>Serving over 17,000,000 API calls each month!</p>
+          </div>
+        </div>
+        <div className="checkDocsContainer">
+          <div className="checkDocs">
+            <p>
+              All the Pokémon data you'll ever need in one place, easily
+              accessible through a modern RESTful API.
+            </p>
+            <p>
+              <Link to="/">Check out the docs!</Link>
+            </p>
+          </div>
         </div>
       </main>
     </div>
